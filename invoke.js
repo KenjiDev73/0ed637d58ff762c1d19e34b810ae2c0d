f118f11f21d0d118513571d3ba2a38cb0638931e
KenjiDev73 <106098930+KenjiDev73@users.noreply.github.com>
2022-05-23 23:13:48 +0700

    Create invoke.js

---
+++ invoke.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+atOptions = {
+'key' : '0ed637d58ff762c1d19e34b810ae2c0d',
+'format' : 'iframe',
+'height' : 250,
+'width' : 300,
+'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://kenjidev73.github.io/0ed637d58ff762c1d19e34b810ae2c0d/invoke.js"></scr' + 'ipt>');
+</script>
